9aec929d69761a57f1a035ced11cfee22130c71f
caiguocheng <244122535@qq.com>
2020-12-25 11:21:53 +0800

    Updates

---
+++ src/components/Content.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import * as ReactBootstrap from "react-bootstrap";
+
+const { Container } = ReactBootstrap;
+
+const Content = (props) => (
+  <div>
+    <Container>{props.children}</Container>
+  </div>
+);
+export default Content;
